beaafd47a347a737ac92d3852a3429b888a42533
gn0mesort <gnomesort@megate.ch>
2017-05-02 10:51:22 -0700

    update to 1.1.0
    Signed-off-by: gn0mesort <gnomesort@megate.ch>

--- plugins/GGreentext/GGreentext.plugin.js
+++ plugins/GGreentext/GGreentext.plugin.js
@@ -6,31 +6,36 @@
     CSS.
 */
 
-var GGreentext = function () { };
+var GGreentext = function () { }
 GGreentext.prototype = {
-    start: function () { this.convert(); },
-    load: function () { },
-    unload: function () { },
-    stop: function () { },
-    onMessage: function () { setTimeout(this.convert(), 2000); },
-    onSwitch: function () { this.convert(); },
-    oberserver: function (e) { },
-    getSettingsPanel: function () { return ''; },
-    getName: function () { return 'GGreentext'; },
-    getDescription: function () { return 'Convert >quotes to greentext.<br /> Styling for greentext must be done in external CSS.'; },
-    getVersion: function () { return '1.0.2'; },
-    getAuthor: function () { return 'gn0mesort'; },
-    convert: function () {
-        $('.message-text .markup').each(function () { //Select elements of the class "markup" belonging to elements of the class "message-text"
-            let target = $(this);
-            if (target.find('code').length === 0) { //Don't bug code tags
-                if (target.attr('handled.gnomesort.greentext') !== true) { //If not handled
-                    target.html(function (_, html) {
-                        return html.replace(/(&gt;\S[^\n<]*)/g, '<span class="greentext">$1</span>'); //Convert to greentext
-                    });
-                    target.attr('handled.gnomesort.greentext', true); //Set handled
-                }
-            }
-        });
+  start: function () { this.convert() },
+  load: function () { },
+  unload: function () { },
+  stop: function () { },
+  onMessage: function () { setTimeout(this.convert(), 2000) },
+  onSwitch: function () { this.convert() },
+  oberserver: function (e) { },
+  getSettingsPanel: function () { return '' },
+  getName: function () { return 'GGreentext' },
+  getDescription: function () { return 'Convert >quotes to greentext.<br /> Styling for greentext must be done in external CSS.'; },
+  getVersion: function () { return '1.1.0' },
+  getAuthor: function () { return 'gn0mesort' },
+  convert: function () {
+    let targets = $('.message-text .markup') // Select targets
+    for (let i = 0; i < targets.length; ++i) { // For each target
+      let target = $(targets[i]) // Select the current target
+      if (!target.attr('handled.gnomesort.greentext') && target.find('code').length === 0) { // If not handled and not a code block
+        let data = target.html().split('\n') // Split html on new line
+        for (let j = 0; j < data.length; ++j) { // For each line
+          if (j === 0) { // On the first line
+            data[j] = data[j].replace(/>(\s*&gt;[^<]+)/g, '><span class="greentext">$1</span>') // Handle html intro and replace
+          } else { // Otherwise
+            data[j] = data[j].replace(/^(\s*&gt;[^<]+)/g, '<span class="greentext">$1</span>') // Replace text with span
+          }
+        }
+        target.html(data.join('\n')) // Rejoin html
+        target.attr('handled.gnomesort.greentext', true) // Set handled
+      }
     }
-};
+  }
+}
--- plugins/GMedia/GMedia.plugin.js
+++ plugins/GMedia/GMedia.plugin.js
@@ -4,52 +4,61 @@
     Builds on the premise of mediaSupport.plugin.js
 */
 
-var GMedia = function () { };
+var GMedia = function () { }
 GMedia.prototype = {
-    start: function () { this.convert(); },
-    load: function () { },
-    unload: function () { },
-    stop: function () { },
-    onMessage: function () { setTimeout(this.convert(), 2000); },
-    onSwitch: function () { this.convert(); },
-    observer: function (e) { },
-    getSettingsPanel: function () { return ''; },
-    getName: function () { return 'GMedia'; },
-    getDescription: function () { return 'Adds HTML5 media support to Discord.<br /> Based on mediaSupport.plugin.js'; },
-    getVersion: function () { return '1.0.3'; },
-    getAuthor: function () { return 'gn0mesort'; },
-    count: 0,
-    convert: function () {
-        let count = this.count; //Count of the number of multimedia files recognized
-        $('.attachment-inner a').each(function () { //For each <a> element with a parent of the class "message"
-            let target = $(this); //This element
-            let href = target.attr('href').replace('http:', 'https:'); //Discord only likes HTTPS
-            let scroller = $('.scroller.messages')[0]; //The message scroller window
-            let scroll = scroller.scrollHeight - scroller.scrollTop === scroller.clientHeight; //Whether or not the user has scrolled away from the most recent message
-
-            if (href !== undefined) { //If href exists
-                let type = href.split('.')[href.split('.').length - 1].toLowerCase(); //Get the file type
-                let fileName = href.split('/')[href.split('/').length - 1]; //Get the file name
-                if (type === 'mp4' || type == 'webm') { //If video
-                    console.log(`Media Found! type is ${type} & href is ${href}`); //Log video found
-                    target.replaceWith(`<video id="video${count}" style="height: 320px; width: 30vw;" src="${encodeURI(href)}" type="video/${type}" controls=""></video>`); //Replace <a> element with <video> element
-                    $('#video' + count++).parent().find('.metadata')[0].innerText += ` - ${fileName} - ${type}`; //Fill video metadata
-                }
-                else if (type === 'mp3' || type === 'ogg' || type === 'wav') { //If audio
-                    if (type === 'mp3') { type = 'mpeg'; } //Convert mp3 to mpeg type
-                    console.log(`Media Found! type is ${type} & href is ${href}`); //Log audio found
-                    target.replaceWith(`<audio id="audio${count}" src="${encodeURI(href)}" type="audio/${type}" controls=""></audio>`); //Replace <a> element with <audio> element
-                    $('#audio' + count++).parent().find('.metadata')[0].innerText += ` - ${fileName} - ${type}`; //Fill audio metadata
-                }
-                else { scroll &= false; } //If not audio or video definitely don't scroll
-            }
-            else { scroll &= false; } //If not defined don't scroll
-            if (scroll) { //If the message window needs to be scrolled 
-                scroller.scrollTop = scroller.scrollHeight; //Scroll to bottom
-                console.log('Scrolling to most recent!'); //Log scrolling
-            }
-        });
-        if (count < 10000) { this.count = count; } //Catch ridiculous counts
-        else { this.count = 0; } //If the count was ridiculous reset it.
+  start: function () { this.convert() },
+  load: function () { },
+  unload: function () { },
+  stop: function () { },
+  onMessage: function () { setTimeout(this.convert(), 2000) },
+  onSwitch: function () { this.convert() },
+  observer: function (e) { },
+  getSettingsPanel: function () { return '' },
+  getName: function () { return 'GMedia' },
+  getDescription: function () { return 'Adds HTML5 media support to Discord.<br /> Based on mediaSupport.plugin.js' },
+  getVersion: function () { return '1.1.0' },
+  getAuthor: function () { return 'gn0mesort' },
+  convert: function () {
+    let targets = $('.attachment-inner a, .markup>a') // Select targets
+    let scroller = $('.scroller.messages')[0] // Select scroller
+    for (let i = 0; i < targets.length; ++i) { // For each targer
+      let target = $(targets[i]) // Select the target
+      let scroll = scroller.scrollHeight - scroller.scrollTop === scroller.clientHeight // Calculate scroll value
+      if (!target.attr('handled.gnomesort.media') && target.attr('href')) { // If there's an href and not handled
+        let href = target.attr('href') // Get the href
+        let type = href.split('.')[href.split('.').length - 1] // Get the file type
+        let fileName = href.split('/')[href.split('/').length - 1] // Get the file name
+        if (type === 'mp4' || type === 'webm') { // If video
+          let video = $(`<video style="height: 320px; width: 30vw;" src="${encodeURI(href)}" type="video/${type}" controls=""></video>`) // Create video element
+          let metaData = null // Declare metadata
+          console.log(`Media Found! type is ${type} & href is ${href}`) // Log
+          target.replaceWith(video) // Replace the link with the video
+          metaData = video.parent().find('.metadata') // Find the metadata element
+          if (metaData.length > 0) { // If the metadata exists
+            metaData[0].innerText += ` - ${fileName} - ${type}` // Fill video metadata
+          } else { // Otherwise
+            video.parent().append(`<div class="metadata"><a href="${href}">${href}</a></div>`) // Create metadata
+          }
+          video.parent().attr('handled.gnomesort.media', true) // Set handled
+        } else if (type === 'mp3' || type === 'ogg' || type === 'wav') { // If audio
+          if (type === 'mp3') { type = 'mpeg' } // Convert mp3 to mpeg type
+          let audio = $(`<audio src="${encodeURI(href)}" type="audio/${type}" controls=""></audio>`) // Create audio element
+          let metaData = null // Declare metadata
+          console.log(`Media Found! type is ${type} & href is ${href}`) // Log
+          target.replaceWith(audio) // Replace link with audio
+          metaData = audio.parent().find('.metadata') // Find the metadata element
+          if (metaData.length > 0) { // If metadata exists
+            metaData[0].innerText += ` - ${fileName} - ${type}` // Fill video metadata
+          } else { // Otherwise
+            audio.parent().append(`<div class="metadata"><a href="${href}">${href}</a></div>`) // Create metadata
+          }
+          audio.parent().attr('handled.gnomesort.media', true) // Set handled
+        } else { scroll = false } // If no file is found don't scroll
+      } else { scroll = false } // If skipping target don't scroll
+      if (scroll) { // If the message window needs to be scrolled
+        scroller.scrollTop = scroller.scrollHeight // Scroll to bottom
+        console.log('Scrolling to most recent!') // Log scrolling
+      }
     }
-};
+  }
+}
