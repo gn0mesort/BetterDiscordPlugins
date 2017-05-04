957047eda20fb5c030b7f7d13c64547383a501c4
gn0mesort <gnomesort@megate.ch>
2017-05-03 21:56:13 -0700

    fix channel switching
    Signed-off-by: gn0mesort <gnomesort@megate.ch>

--- plugins/GSpellCheck/GSpellCheck.plugin.js
+++ plugins/GSpellCheck/GSpellCheck.plugin.js
@@ -9,7 +9,7 @@ var GSpellCheck = function () { }
 GSpellCheck.prototype = {
   getName: function () { return 'GSpellCheck' },
   getDescription: function () { return 'Add spellchecking with Typo.js' },
-  getVersion: function () { return '1.0.0' },
+  getVersion: function () { return '1.0.1' },
   getAuthor: function () { return 'gn0mesort' },
   start: function () {
     // Load Typo object using LibreOffice dictionary
@@ -20,22 +20,7 @@ GSpellCheck.prototype = {
     console.log(`Typo Loaded with dictionary ${this.spellChecker.typo.dictionary} from ${this.spellChecker.typo.settings.dictionaryPath}`)
     console.log(`mispeling is ${this.spellChecker.spellCheck('mispeling') ? 'correct' : 'misspelled'}`)
     webFrame.setSpellCheckProvider('en-US', true, this.spellChecker) // start provider
-    $('textarea').on('contextmenu.gnomesort', { spellChecker: this.spellChecker }, function (event) { // set context menu event
-      let selection = window.getSelection().toString() // get selected text
-      let suggestions = event.data.spellChecker.typo.suggest(selection) // get suggestions
-      if (suggestions.length > 0) { // If suggestions are found
-        event.stopPropagation() // If suggestion menu is open don't do any more menus
-        let spellerMenu = new Menu() // Create the context menu
-        spellerMenu.clickEvent = function (menuItem) { // Click function for menu options. Simply replaces the word inline
-          let area = $(window.getSelection().anchorNode.childNodes[window.getSelection().anchorOffset])
-          area.val(area.val().slice(0, area[0].selectionStart) + menuItem.label + area.val().slice(area[0].selectionEnd, area.val().length))
-        }
-        for (let suggestion of suggestions) { // Add each option to the context menu
-          spellerMenu.append(new MenuItem({ label: suggestion, click: spellerMenu.clickEvent }))
-        }
-        spellerMenu.popup() // Display context menu
-      }
-    })
+    $('textarea').on('contextmenu.gnomesort', { spellChecker: this.spellChecker }, this.contextMenu) // Enable context menu
   },
   stop: function () {
     $('textarea').off('contextmenu.gnomesort') // Disable context menu
@@ -44,7 +29,10 @@ GSpellCheck.prototype = {
   load: function () { },
   unload: function () { },
   onMessage: function () { },
-  onSwitch: function () { },
+  onSwitch: function () {
+    $('textarea').off('contextmenu.gnomesort') // Disable context menu
+    $('textarea').on('contextmenu.gnomesort', { spellChecker: this.spellChecker }, this.contextMenu) // Reenable
+  },
   observer: function (e) { },
   spellChecker: { // SpellChecker object
     typo: null, // The Typo object that underlies this SpellChecker
@@ -54,6 +42,22 @@ GSpellCheck.prototype = {
   },
   spellCheckerDisable: { // Disable checking by always returning true
     spellCheck: function (text) { return true }
+  },
+  contextMenu: function (event) { // set context menu event
+    let selection = window.getSelection().toString() // get selected text
+    let suggestions = event.data.spellChecker.typo.suggest(selection) // get suggestions
+    if (suggestions.length > 0) { // If suggestions are found
+      event.stopPropagation() // If suggestion menu is open don't do any more menus
+      let spellerMenu = new Menu() // Create the context menu
+      spellerMenu.clickEvent = function (menuItem) { // Click function for menu options. Simply replaces the word inline
+        let area = $(window.getSelection().anchorNode.childNodes[window.getSelection().anchorOffset])
+        area.val(area.val().slice(0, area[0].selectionStart) + menuItem.label + area.val().slice(area[0].selectionEnd, area.val().length))
+      }
+      for (let suggestion of suggestions) { // Add each option to the context menu
+        spellerMenu.append(new MenuItem({ label: suggestion, click: spellerMenu.clickEvent }))
+      }
+      spellerMenu.popup() // Display context menu
+    }
   }
 }
 
