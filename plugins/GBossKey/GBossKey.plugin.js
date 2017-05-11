c548c36189c7ac7cb4309763397ce51da37cad56
gn0mesort <gnomesort@megate.ch>
2017-05-11 15:05:40 -0700

    Load keycodes from file in GBosskey
    Signed-off-by: gn0mesort <gnomesort@megate.ch>

--- plugins/GBossKey/GBossKey.plugin.js
+++ plugins/GBossKey/GBossKey.plugin.js
@@ -5,23 +5,21 @@
 var GBossKey = function () { }
 GBossKey.prototype = {
   start: function () {
-    if (!this.keyCodes) {
-      let plugin = this
-      $.ajax({
-        url: 'https://raw.githubusercontent.com/gn0mesort/BetterDiscordPlugins/master/plugins/GBossKey/keys.json',
-        async: false
-      }).done(function (data) {
-        console.log(this)
-        console.log(data.responseText)
-        this.keyCodes = JSON.parse(data.responseText)
-      })
-    }
     let configKey = bdPluginStorage.get('GBossKey', 'key')
     if (configKey) { this.key = configKey }
     $(document).on('keyup.gnomesort', { key: this.key }, this.hideWindow)
   },
   stop: function () { $(document).off('keyup.gnomesort') },
-  load: function () { },
+  load: function () { 
+    let plugin = this
+    $.ajax({
+      url: 'https://raw.githubusercontent.com/gn0mesort/BetterDiscordPlugins/master/plugins/GBossKey/keys.json',
+      async: false,
+      dataType: 'json'
+    }).done(function (data) {
+      plugin.keyCodes = data
+    })
+  },
   getName: function () { return 'GBossKey' },
   getDescription: function () { return 'Press a key to minimize Discord. Any key may be bound using this plugin\'s settings panel.' },
   getVersion: function () { return '1.0.3' },
